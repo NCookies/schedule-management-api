dfed313bd4bfe7f3440d3f7feb93cba792a3a0cc
SeungWoo Ryu <swstar21c@gmail.com>
2025-03-20 16:02:18 +0900

    #1 - 프로젝트 패키지 이름 수정
    - 패키지 이름이 너무 길어서 앞 글자만 따서 줄였다.

--- src/main/java/xyz/ncookie/sma/ScheduleManagementApiApplication.java
+++ src/main/java/xyz/ncookie/sma/ScheduleManagementApiApplication.java
@@ -1,4 +1,4 @@
-package xyz.ncookie.schedulemanagementapi;
+package xyz.ncookie.sma;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/xyz/ncookie/sma/ScheduleManagementApiApplicationTests.java
+++ src/test/java/xyz/ncookie/sma/ScheduleManagementApiApplicationTests.java
@@ -1,4 +1,4 @@
-package xyz.ncookie.schedulemanagementapi;
+package xyz.ncookie.sma;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
