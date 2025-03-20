664e546f978bce5a23064039cc9a93f7d9b739b4
SeungWoo Ryu <swstar21c@gmail.com>
2025-03-20 15:37:34 +0900

    #1 - 프로젝트 생성 및 gitignore 작성

---
+++ src/main/java/xyz/ncookie/schedulemanagementapi/ScheduleManagementApiApplication.java
@@ -0,0 +1,13 @@
+package xyz.ncookie.schedulemanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleManagementApiApplication.class, args);
+    }
+
+}
