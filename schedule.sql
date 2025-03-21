9364494581cf2f1288f3774e9e87fd6262d3d35f
SeungWoo Ryu <swstar21c@gmail.com>
2025-03-21 15:35:50 +0900

    feat/#5 - user 테이블 생성 및 연관관계 SQL 작성
    - `user`의 기본키를 `schedule` 테이블에서 외래키로 참조하여 연관관계를 생성했다.
    - 아직까지는 `user` 관련해서 본격적으로 구현되지 않았기 때문에 `user` 데이터 삭제 시 `schedule` 데이터도 함께 삭제하는 것(`ON DELETE CASCADE`이 아니라 그냥 NULL 로 세팅하도록 했다.

--- schedule.sql
+++ schedule.sql
@@ -2,11 +2,24 @@ CREATE DATABASE IF NOT EXISTS scheduler;
 
 USE scheduler;
 
-CREATE TABLE schedule (
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS schedule;
+
+CREATE TABLE user (
     id              BIGINT AUTO_INCREMENT PRIMARY KEY,
-    task            VARCHAR(500) NOT NULL,  -- 할 일
-    author          VARCHAR(50) NOT NULL,   -- 작성자명
+    name            VARCHAR(50) NOT NULL,   -- 이름
+    email           VARCHAR(50) NOT NULL UNIQUE,    -- 이메일
     password        VARCHAR(255) NOT NULL,  -- 비밀번호 (해시)
     created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    -- 작성일
     modified_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP     -- 수정일
+);
+
+CREATE TABLE schedule (
+    id              BIGINT AUTO_INCREMENT PRIMARY KEY,
+    user_id         BIGINT,   -- 사용자 식별자(id) 외래키
+    task            VARCHAR(255) NOT NULL,  -- 할 일
+    created_at      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    -- 작성일
+    modified_at     TIMESTAMP DEFAULT CURRENT_TIMESTAMP,    -- 수정일
+    FOREIGN KEY (user_id) REFERENCES user(id)
+        ON DELETE SET NULL
 );
